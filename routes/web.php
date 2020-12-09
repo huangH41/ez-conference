b3fe8b4aefb6cb8588a9b27989725594ea292984
AlbertZasura <albertalbert841@gmail.com>
2020-12-09 16:09:48 +0700

    Schedule List Component

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/schedule', function () {
+    return view('scheduleList');
+});
+
