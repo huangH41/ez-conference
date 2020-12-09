4f7c2e66348ac031f31bc51ead2ba6a0a0d54c6f
AlbertZasura <albertalbert841@gmail.com>
2020-12-09 11:07:42 +0700

    Seeding & Migration

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'username' => "member",
+                'email' => "member@gmail.com",
+                'password' => bcrypt("member")
+            ],
+            [
+                'username' => "albert",
+                'email' => "albert@gmail.com",
+                'password' => bcrypt("albert")
+            ],
+        ]);
+    }
+}
