0b033ea8b3cf8c8469589156a6b5ad971cacfa92
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-12-15 13:17:59 +0700

    Change sidebar namings

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="sidebar">
     <div class="sidebar-icon active">
-        <a class="sidebar-icon-yield" href="{{route('home')}}">
+        <a class="sidebar-icon-container" href="{{route('home')}}">
             <img src="/assets/icons/home.svg" alt="Home Icon">
         </a>
     </div>
     <div class="sidebar-icon">
-        <a class="sidebar-icon-yield" href="">
+        <a class="sidebar-icon-container" href="">
             <img src="/assets/icons/rent.svg" alt="Home Icon">
         </a>
     </div>
