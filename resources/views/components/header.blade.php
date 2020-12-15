53aa73ef73916d6f15677f0d0c5cf4998c4265b4
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-12-15 14:24:32 +0700

    Make a profile dropdown on the header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,10 +6,34 @@
             @yield('title')
         </div>
     </div>
-    <div class="profile">
-        <div class="body2 profile-name">
-            Hello! Username
+    @auth
+    <div role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div class="profile">
+            <div class="body2 profile-name">
+                Hello! {{ Auth::user()->username }}
+            </div>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <div class="dropdown-item-text body1">
+                    <div class="d-flex align-items-center">
+                        <img src="/assets/images/profile-picture.png" class="profile-picture profile-picture-detail"/>
+                        <div class="container-fluid">
+                            <strong class="body1">{{ Auth::user()->username }}</strong>
+                            <div>{{ Auth::user()->email }}</div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <a class="dropdown-item body1" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+            <img src="/assets/images/profile-picture.png" class="profile-picture"/>
         </div>
-        <img src="/assets/images/profile-picture.png" class="profile-picture">
     </div>
+    @endauth
 </div>
