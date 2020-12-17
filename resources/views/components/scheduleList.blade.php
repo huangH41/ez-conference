4114e13ab203190e702e2d2f2076e53760a105e7
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-12-17 14:58:02 +0700

    Set responsive without hiding navbar

--- resources/views/components/scheduleList.blade.php
+++ resources/views/components/scheduleList.blade.php
@@ -6,10 +6,10 @@
 
     {{-- Card --}}
     @foreach($rents as $rent)
-        <div class="card mb-4 p-3"
+        <div class="card mb-4"
              style="max-width: 100%; border-radius: 8px; border:none; box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0), 0 0px 20px 0 rgba(0, 0, 0, 0.19);">
-            <div class="card-body row g-0">
-                <div class="col-md-3 px-3 d-flex align-items-center" style="border-right:1px solid #a8a8a8">
+            <div class="card-body row p-4">
+                <div class="col-3 px-3 d-flex align-items-center" style="border-right:1px solid #a8a8a8">
                     <img src="{{asset('/assets/zoom-logo.png')}}" style="max-width: 100%" alt="zoom">
                 </div>
                 <div class="col my-auto d-flex justify-content-between">
@@ -18,29 +18,30 @@
                         <p class="card-text body1 m-0">Participant Limit: 100</p>
                         <p class="card-text body1 m-0">Rental Start Time: {{date('h:i A',strtotime($rent->time))}}</p>
                     </div>
-                    <div class="d-flex align-items-center">
-                        @if($rent->join_link != null && $rent->start_link != null)
-                            <div class="p-1">
-                                <a href="{{$rent->start_link}}" type="button" style="background-color: #fd367e; border:none"
-                                   class="subheader2 btn btn-primary" target="_blank">
-                                    Host Link
-                                </a>
-                            </div>
-                            <div class="p-1">
-                                <a href="{{$rent->join_link}}" type="button" style="background-color: #fd367e; border:none"
-                                   class="subheader2 btn btn-primary" target="_blank">
-                                    Guest Link
-                                </a>
-                            </div>
-                    </div>
-                    @elseif(date('j M Y',strtotime($rent->date)) == date('j M Y',time()))
-                        <a href="{{route('createZoom',$rent->id)}}" type="button"
-                           style="background-color: #fd367e; border:none" class="subheader2 btn btn-primary">
-                            Create Meetings
-                        </a>
-                    @endif
                 </div>
+                @if($rent->join_link != null && $rent->start_link != null)
+                <div class="col py-2 d-flex align-items-center justify-content-center">
+                    @if($rent->join_link != null && $rent->start_link != null)
+                        <div class="p-1">
+                            <a href="{{$rent->start_link}}" type="button" style="background-color: #fd367e; border:none"
+                               class="body2 btn btn-primary" target="_blank">
+                                Host Link
+                            </a>
+                        </div>
+                        <div class="p-1">
+                            <a href="{{$rent->join_link}}" type="button" style="background-color: #fd367e; border:none"
+                               class="body2 btn btn-primary" target="_blank">
+                                Guest Link
+                            </a>
+                        </div>
+                    @elseif(date('j M Y',strtotime($rent->date)) == date('j M Y',time()))
+                    <a href="{{route('createZoom',$rent->id)}}" type="button"
+                       style="background-color: #fd367e; border:none" class="body2 btn btn-primary">
+                        Create Meetings
+                    </a>
+                    @endif
             </div>
+            @endif
         </div>
 </div>
 @endforeach
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,10 @@
                 <div class="alert alert-success body1">{{session()->get('success')}}</div>
             @endif
             <div class="row mb-4">
-                <div class="col-md-7">
+                <div class="col-md-7 mb-3">
                     @include('components.calendar',['rents' => $rents])
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-5 mb-3">
                     @include('components.counter',['rentsCount' => $rentsCount])
                 </div>
             </div>
