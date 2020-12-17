9286d680371f5ab7b38a70c888692cbf1e156b41
Herman <huangherman41@gmail.com>
2020-12-18 04:25:35 +0700

    Create transaction view

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -18,7 +18,18 @@
 
         <div class="row mb-4">
             <div class="col-md-5">
-                
+                <div class="transaction-price__container">
+                  <div class="transaction-price__content">
+                    <h1 class="subheader2">Rental Price</h1>
+                    <h1 class="hero">30.000</h1>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-md-7">
+              <button type="submit" class="button button--main">
+                Create Rental Schedule
+              </button>
             </div>
         </div>
       </div>
