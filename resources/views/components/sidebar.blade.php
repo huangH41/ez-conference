63f21b6f4ad6b573d0334cff28ceb13f3570dcd6
Surya <suryashen28@gmail.com>
2020-12-28 15:17:31 +0700

    added FE for admin

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,12 +1,15 @@
 <div id="sidebar" class="sidebar">
+    @if(Auth::user()->role == 'user')
     <div class="sidebar-icon @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'home') active @endif">
         <a class="sidebar-icon-container" href="{{route('home')}}">
             <img src="/assets/icons/home.svg" alt="Home Icon">
         </a>
     </div>
-    <div class="sidebar-icon @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'rent') active @endif">
-        <a class="sidebar-icon-container" href="{{route('rent')}}">
-            <img src="/assets/icons/rent.svg" alt="Home Icon">
+    @endif
+    <div class="sidebar-icon @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'rent' or
+        \Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'zoomIndex') active @endif">
+        <a class="sidebar-icon-container" href="@if(Auth::user()->role == 'admin') {{route('zoomIndex')}} @else {{route('rent')}} @endif">
+            <img src="/assets/icons/rent.svg" alt="Rent Icon">
         </a>
     </div>
 </div>
